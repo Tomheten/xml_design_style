0b34eaea17b15ed46be7e94137d28b83511bec08
boris_medoshin <jetun@ya.ru>
2024-06-21 17:16:35 +0300

    mutation observer

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 var parser = new DOMParser();
-var xml = "<note>\n<to>Tove</to>\n<from>Jani</from>\n<heading>Reminder</heading>\n<body>Don't forget me this weekend!</body>\n</note>";
-var xmlDoc = parser.parseFromString(xml, 'text/xml');
-console.log('-----xml', xmlDoc);
+var xmlSrc = "<note>\n<to>Tove</to>\n<from>Jani</from>\n<heading>Reminder</heading>\n<body>Don't forget me this weekend!</body>\n</note>";
+var xmlDoc = parser.parseFromString(xmlSrc, 'text/xml').documentElement;
+console.dir(xmlDoc);
+var serializer = new XMLSerializer();
+var xmlStr = serializer.serializeToString(xmlDoc);
+console.log('-------- ser xml', xmlStr);
+var observer = new MutationObserver(function (mutationRecords) {
+    console.log(mutationRecords); // console.log(изменения)
+});
+// наблюдать за всем, кроме атрибутов
+observer.observe(xmlDoc, {
+    attributes: true,
+    characterData: true,
+    childList: true, // наблюдать за непосредственными детьми
+    subtree: true, // и более глубокими потомками
+    characterDataOldValue: true // передавать старое значение в колбэк
+});
+var body = xmlDoc.querySelector('body');
+body.append(document.createTextNode('sdf'));
+document.body.append(xmlDoc);
--- main.ts
+++ main.ts
@@ -5,5 +5,27 @@ const xmlSrc = `<note>
 <heading>Reminder</heading>
 <body>Don't forget me this weekend!</body>
 </note>`;
-let xmlDoc = parser.parseFromString(xmlSrc, 'text/xml');
-console.log('-----xml', xmlDoc);
+let xmlDoc = parser.parseFromString(xmlSrc, 'text/xml').documentElement;
+console.dir(xmlDoc);
+
+const serializer = new XMLSerializer();
+const xmlStr = serializer.serializeToString(xmlDoc);
+console.log('-------- ser xml', xmlStr)
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords); // console.log(изменения)
+});
+
+// наблюдать за всем, кроме атрибутов
+observer.observe(xmlDoc, {
+    attributes: true,
+    characterData: true,
+    childList: true, // наблюдать за непосредственными детьми
+    subtree: true, // и более глубокими потомками
+    characterDataOldValue: true // передавать старое значение в колбэк
+});
+
+const body = xmlDoc.querySelector('body')
+body.append(document.createTextNode('sdf'))
+
+document.body.append(xmlDoc)
